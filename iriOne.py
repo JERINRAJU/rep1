3d09937a8b42dcbbd8ef670fb77d58d38e74bffb
JERINRAJU <jerinraju@cs.ajce.in>
2017-10-24 14:15:12 +0530

    Add files via upload

--- iriOne.py
+++ iriOne.py
@@ -1,3 +1,37 @@
+from scipy.spatial import distance
+
+#
+#
+def eucl(a, b):
+	return distance.euclidean(a,b)
+class myKNN():
+	def fit(self, x_train,y_train):     #calssifier method
+		self.x_train = x_train
+		self.y_train = y_train
+
+	def predict(self, x_train):   #predict method of classifier
+		prediction = []
+		for row in x_test:
+			label = self.closest(row)
+			prediction.append(label)
+		return prediction
+
+	def closest(self,row):
+		best_dist = eucl(row,self.x_train[0])
+		best_index = 0
+		for i in range(1, len(self.x_train)):
+			dist = eucl(row, self.x_train[i])
+			if dist < best_dist:
+				best_dist = dist
+				best_index = 1
+			return self.y_train[best_index]
+
+
+
+
+
+
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 
@@ -18,8 +52,22 @@ print("accuracy=",accuracy_score(y_test,p))
 
 
 from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier()
-knn.fit(x_train, y_train)    
+knn = myKNN()
+knn.fit(x_train, y_train)
 p = knn.predict(x_test)
-
+ 
 print("accuracy=",accuracy_score(y_test,p))
+
+
+
+
+
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+p = lr.predict(x_test)
+ 
+print("accuracy=",accuracy_score(y_test,p))
+
+
